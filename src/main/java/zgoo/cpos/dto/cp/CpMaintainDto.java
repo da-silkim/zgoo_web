af4b04467065caa2116bee90e63104cc56cddfd3
ghchoi-dongah <ghchoi@dongahelecomm.co.kr>
2025-02-24 16:17:00 +0900

    충전기 장애 관리 DTO 및 Entity 추가

---
+++ src/main/java/zgoo/cpos/dto/cp/CpMaintainDto.java
@@ -0,0 +1,57 @@
+package zgoo.cpos.dto.cp;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+public class CpMaintainDto {
+
+    @Data
+    @SuperBuilder(toBuilder = true)
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    public static class CpMaintainRegDto {
+        private Long cpmaintainId;
+        private String chargerId;
+        private LocalDateTime regDt;
+        private String errorType;
+        private String errorContent;
+        private String pictureLoc1;
+        private String pictureLoc2;
+        private String pictureLoc3;
+        private LocalDateTime processDate;
+        private String processStatus;
+        private String processContent;
+        private String regUserId;
+    }
+
+    @Data
+    @SuperBuilder(toBuilder = true)
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    public static class CpInfoDto {
+        private Long companyId;
+        private String companyName;
+        private String stationId;
+        private String stationName;
+        private String address;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @SuperBuilder
+    @EqualsAndHashCode(callSuper = true)
+    @ToString(callSuper = true)
+    public static class CpMaintainListDto extends CpMaintainRegDto {
+        private Long companyId;
+        private String companyName;
+        private String errorTypeName;
+        private String processStatusName;
+    }
+}
