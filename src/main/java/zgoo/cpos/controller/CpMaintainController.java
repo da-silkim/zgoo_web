f1e6bcf8727a4df279e66360a30e75fa234e67c8
ghchoi-dongah <ghchoi@dongahelecomm.co.kr>
2025-02-27 09:49:12 +0900

    충전기 장애 관리 단건조회/삭제기능 추가

--- src/main/java/zgoo/cpos/controller/CpMaintainController.java
+++ src/main/java/zgoo/cpos/controller/CpMaintainController.java
@@ -7,11 +7,11 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
@@ -51,6 +51,38 @@ public class CpMaintainController {
         }
     }
 
+    // 단건 조회
+    @GetMapping("/get/{cpmaintainId}")
+    public ResponseEntity<Map<String, Object>> findMaintainOne(@PathVariable("cpmaintainId") Long cpmaintainId) {
+        log.info("=== find maintain info ===");
+
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            CpMaintainRegDto maintainFineOne = this.cpMaintainService.findMaintainOne(cpmaintainId);
+
+            if (maintainFineOne == null) {
+                response.put("message", "등록된 충전기 장애 정보가 없습니다.");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+            
+            CpInfoDto cpInfo = this.cpMaintainService.searchCsCpInfo(maintainFineOne.getChargerId());
+
+            if (cpInfo.getChargerId() == null) {
+                response.put("message", "등록된 충전기ID 정보가 없습니다.");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+            }
+
+            response.put("cpMaintain", maintainFineOne);
+            response.put("cpInfo", cpInfo);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            log.error("[findMaintainOne] error: {}", e.getMessage());
+            response.put("message", "충전기 장애 정보 조회 중 오류가 발생했습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
     // 등록
     @PostMapping("/new")
     public ResponseEntity<String> createMaintain(@ModelAttribute @Valid CpMaintainRegDto dto, Principal principal) {
@@ -63,6 +95,26 @@ public class CpMaintainController {
             log.error("[createMaintain] error: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                                     .body("충전기 장애 정보 등록 중 오류 발생");
+        }
+    }
+
+    @DeleteMapping("/delete/{cpmaintainId}")
+    public ResponseEntity<String> deleteMaintain(@PathVariable("cpmaintainId") Long cpmaintainId) {
+        log.info("=== delete maintain info ===");
+
+        if (cpmaintainId == null) {
+            log.error("cpmaintainId id null");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                    .body("충전기 장애 정보ID가 없습니다.");
+        }
+
+        try {
+            this.cpMaintainService.deleteMaintain(cpmaintainId);
+            return ResponseEntity.ok("충전기 장애 정보가 정상적으로 삭제되었습니다.");
+        } catch (Exception e) {
+            log.error("[deleteMaintain] error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                    .body("충전기 장애 정보 삭제 중 오류 발생");
         }
     }
 }
