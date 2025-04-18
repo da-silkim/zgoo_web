75b329be99979eef5cd9b46b9957fcc2e47f6dbc
ghchoi-dongah <ghchoi@dongahelecomm.co.kr>
2025-04-18 12:21:32 +0900

    매입관리 충전소 조회 화면구성

--- src/main/resources/static/js/calc/purchase.js
+++ src/main/resources/static/js/calc/purchase.js
@@ -28,4 +28,44 @@ $(document).ready(function() {
             });
         }
     });
+
+    $('#stationOpSearchBtn').on('click', function() {
+        const searchStation = document.getElementById('searchStation').value;
+        const stationOpSearch = document.getElementById('stationOpSearch').value;
+        $.ajax({
+            url: `/purchase/search/station`,
+            method: 'GET',
+            data: {searchOp: searchStation, searchContent: stationOpSearch},
+            success: function(response) {
+                var stationSearchList = document.getElementById('stationSearchList');
+                stationSearchList.innerHTML = '';
+                if (response.csList.length === 0) {
+                    stationSearchList.innerHTML = '<tr><td colspan="3">조회된 데이터가 없습니다.</td></tr>';
+                    return;
+                }
+
+                response.csList.forEach(function (cs) {
+                    var row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${cs.statinName}</td>
+                        <td>${cs.stationId}</td>
+                        <td><button type="button" class="btn btn-outline-grey select-biz-btn" data-station-id="${cs.stationId}" data-station-name="${biz.stationName}">선택</button></td>
+                    `;
+                    stationSearchList.appendChild(row);
+                });
+            },
+            error: function(xhr) {
+                var response = JSON.parse(xhr.responseText);
+                alert(response.message);
+            }
+        });
+    });
+
+    document.getElementById('stationSearchBtn').addEventListener('click', function () {
+        var stationSearchModal = new bootstrap.Modal(document.getElementById('stationSearchModal'), {
+            backdrop: 'static',
+            keyboard: false
+        });
+        stationSearchModal.show();
+    });
 });
